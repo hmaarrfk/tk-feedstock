9a6bbad7d45605fcb23effae77fde4574c66c68f
Mark Harfouche <mark.harfouche@gmail.com>
2019-02-13 00:29:23 -0500

    su?

--- .azure-pipelines/build_steps.sh
+++ .azure-pipelines/build_steps.sh
@@ -30,7 +30,7 @@ run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/su -c 'yum install -y libX11-devel'
+su -c 'yum install -y libX11-devel'
 
 
 # make the build number clobber
