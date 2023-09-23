34915122886720077f7838cbf8166c149de1b020
Uwe L. Korn <uwe.korn@quantco.com>
2023-09-23 21:50:53 +0200

    nmake nmakehlp.exe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -17,6 +17,7 @@ if "%ARCH%"=="32" (
 )
 
 pushd tcl%PKG_VERSION%\win
+nmake nmakehlp.exe
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% release
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% MACHINE=%MACHINE% install
 if %ERRORLEVEL% GTR 0 exit 1
