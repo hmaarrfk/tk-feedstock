082d9b25f238c326cb14f1ceeac5c51a98fdf9d7
Isuru Fernando <isuruf@gmail.com>
2023-11-04 11:12:16 -0500

    Use --enable-aqua only on macos

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,27 +4,25 @@ set -ex
 
 IFS="." read -a VER_ARR <<<"${PKG_VERSION}"
 
-ARCH_FLAG=""
-if [[ ${ARCH} == 64 ]]; then
-    ARCH_FLAG="--enable-64bit"
-fi
 
 pushd tcl${PKG_VERSION}/unix
   # autoreconf -vfi
-  ./configure  --prefix="${PREFIX}"  \
-               --host=${HOST}        \
-               ${ARCH_FLAG}
+  ./configure  --prefix="${PREFIX}"
   make -j${CPU_COUNT} ${VERBOSE_AT}
   make install install-private-headers
 popd
 
+if [[ "$target_platform" == osx-* ]]; then
+  CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-aqua=yes"
+elif [[ "$tk_variant" == xft ]]; then
+  CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-xft"
+fi
+
 pushd tk${PKG_VERSION}/unix
   # autoreconf -vfi
   ./configure --prefix="${PREFIX}"        \
-              --host=${HOST}              \
               --with-tcl="${PREFIX}"/lib  \
-              --enable-aqua=yes           \
-              ${ARCH_FLAG}
+              ${CONFIGURE_ARGS}
   cat config.log
   make -j${CPU_COUNT} ${VERBOSE_AT}
   make install
